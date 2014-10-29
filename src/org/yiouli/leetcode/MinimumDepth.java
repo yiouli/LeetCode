8d4de5315f215ec103cd9c767207932e84bd810f
Yiou Li <liyiousu@gmail.com>
2014-10-29 10:16:42 -0700

    Minimum Depth and Search 2D Matrix solutions.

---
+++ src/org/yiouli/leetcode/MinimumDepth.java
@@ -0,0 +1,52 @@
+/**
+ * @author Yiou Li
+ */
+package org.yiouli.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from 
+ * the root node down to the nearest leaf node.
+ * 
+ * @see <a href="https://oj.leetcode.com/problems/minimum-depth-of-binary-tree/">Minimum Depth on LeetCode</a>
+ */
+public class MinimumDepth {
+	
+	/**
+	 * Definition for binary tree
+	 */
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	 
+	public int minDepth(TreeNode root) {
+		if (root == null) return 0;
+		LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+		q.addLast(root);
+		int depth = 1, lvl = 1, nextLvl = 0;
+		while (q.size() > 0) {
+			TreeNode node = q.removeFirst();
+			lvl--;
+			if (node.left == null && node.right == null) break;
+			if (node.left != null) {
+				q.addLast(node.left);
+				nextLvl++;
+			}
+			if (node.right != null) {
+				q.addLast(node.right);
+				nextLvl++;
+			}
+			if (lvl == 0) {
+				lvl = nextLvl;
+				depth++;
+				nextLvl = 0;
+			}
+		}
+		return depth;
+	}
+}
---
+++ src/org/yiouli/leetcode/Search2DMatrix.java
@@ -0,0 +1,44 @@
+/**
+ * @author Yiou Li
+ */
+package org.yiouli.leetcode;
+
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. 
+ * This matrix has the following properties:
+ * 
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous row.
+ * 
+ * For example,
+ * Consider the following matrix:
+ * [
+ *  [1,   3,  5,  7],
+ *  [10, 11, 16, 20],
+ *  [23, 30, 34, 50]
+ * ]
+ * Given target = 3, return true.
+ * 
+ * @see <a href="https://oj.leetcode.com/problems/search-a-2d-matrix/">Search 2D Matrix on LeetCode</a>
+ */
+public class Search2DMatrix {
+	private int compare(int[][] matrix, long idx, int target) {
+		int n = matrix[0].length;
+		int row = (int)(idx/n), col = (int)(idx%n);
+		return matrix[row][col]-target;
+	}
+
+	private boolean binarySearch(int[][] matrix, long start, long end, int target) {
+		if (start > end) return false;
+		long mid = (start+end)/2;
+		int compare = compare(matrix, mid, target);
+		if (compare == 0) return true;
+		else if (compare > 0) return binarySearch(matrix, start, mid-1, target);
+		else return binarySearch(matrix, mid+1, end, target);
+	}
+
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+		return binarySearch(matrix, 0, (long)matrix.length * matrix[0].length - 1, target);
+	}
+}
